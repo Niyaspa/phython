88fedd6bfc16af14cfc2b9a8f442e5af69687334
Niyaspa <niyaspa8342@gmail.com>
2024-09-30 23:55:46 -0700

    Add files via upload

---
+++ area.py
@@ -0,0 +1,17 @@
+#circle#
+pi=3.14
+r=int(input("enter the radius"))
+c=pi*r*r
+print("area of the circle" ,c)
+
+#rectangle#
+l=int(input("enter the length of rectangle"))
+b=int(input("enter the breadth of rectangle"))
+re=l*b
+print("area of rectangle",re)
+
+#triangle#
+b=int(input("enter the base of the traingle"))
+h=int(input("enter the height of the triangle"))
+tri=(b*h)/2
+print("area of traingle",tri)
