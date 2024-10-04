b0956544db85180af2ab32a0cbbd78b9b716d50f
Niyaspa <niyaspa8342@gmail.com>
2024-10-03 22:51:20 -0700

    Add files via upload

---
+++ markavg.py
@@ -0,0 +1,10 @@
+_4_10-2024
+s1=int(input("enter the first subject mark :"))
+s2=int(input("enter the second subject mark :"))
+s3=int(input("enter the third subject mark :"))
+s4=int(input("enter the forth subject mark :"))
+s5=int(input("enter the fifth subject mark :"))
+total=s1+s2+s3+s4+s5
+print("sum=",total)
+avg=total/5
+print("average=",avg)
