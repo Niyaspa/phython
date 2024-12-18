c7df2bb5d74fa0e4db97502fedf67ed7e624e477
Niyaspa <niyaspa8342@gmail.com>
2024-12-18 13:58:26 +0530

    Add files via upload

---
+++ 18-12-24/math-module/math-module/Geometry/area.py
@@ -0,0 +1,12 @@
+    #area.py
+
+def rectangle_area(length,width):
+    return length * width
+
+def circle_area(radius):
+    from math import pi
+    return pi * radius * radius
+
+def triangle_area(base, height):
+    return 0.5 * base * height
+
